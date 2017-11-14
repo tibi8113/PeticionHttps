d5b7d9bd6be85bc26f6b0e96b79ac23b1c6b4204
tibi8113 <ii.tibi@hotmail.com>
2017-11-14 13:03:50 +0100

    Commit 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #736b6f;
                 padding: 0 25px;
                 font-size: 12px;
                 font-weight: 600;
